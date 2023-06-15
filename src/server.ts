a2e320379dd67e75e6ed67e4a826f0ba60cad00e
Starz0r <starz0r@starz0r.com>
2023-06-14 23:05:45 -0500

    server: organized code into a main function for clarity

--- src/server.ts
+++ src/server.ts
@@ -26,37 +26,26 @@ let config: Config = require('./config/config.json');
 
 /** Exit codes for fatal errors. */
 enum ExitCode {
+    SUCCESS = 0,
     /** Database initialization failed. */
     DB_INIT_FAIL = 1,
     /** S3 Object storage initialization failed. */
     S3_INIT_FAIL = 2
 }
 
-//self executing async function that allows the initialization to halt if a
-//fatal error occurs in an asynchronous function (like database init)
-(async () => {
+async function main(): Promise<number> {
 
     console.log('Welcome to delfruit server 2.0!');
 
-    try {
-        await Database.init();
-    } catch (e) {
-        console.error("Database initialization failed! Is it running? check "+config.db.host);
-        console.error(e);
-        process.exit(ExitCode.DB_INIT_FAIL);
-    }
+    // TODO: database initialization step, currently assumed database is already initialized 
 
     console.log('Initializing express...');
 
-const app = express();
-/*app.use(function (req,res,next) {
-console.log(req.originalUrl);
-next();
-    });*/
+    const app = express();
 
     app.use(cors())
 
-    app.use(bodyParser.json({type:"application/json"}));
+    app.use(bodyParser.json({ type: "application/json" }));
 
     console.log('Initializing jwt middleware...');
 
@@ -68,14 +57,14 @@ next();
 
     console.log('Initializing role middleware...');
 
-    app.use((req,res,next) => {
+    app.use((req, res, next) => {
         if (req.user) {
             req.user.roles = ['game_update'];
         }
         next();
     });
 
-    const e: ErrorRequestHandler = (err,req,res,next) => {
+    const e: ErrorRequestHandler = (err, req, res, next) => {
         if (err && err.name && err.name === 'UnauthorizedError') {
             //invalid token, jwt middleware returns more info in the err
             //but we don't want the client to see
@@ -97,21 +86,21 @@ next();
     // DEBUG ERROR LOGGING
     app.use((req, res, next) => {
         var oldWrite = res.write,
-        oldEnd = res.end;
+            oldEnd = res.end;
 
         var chunks: any[] = [];
 
-        res.write = function (chunk: any) {
+        res.write = function(chunk: any) {
             chunks.push(chunk);
             // @ts-ignore
             return oldWrite.apply(res, arguments);
         };
 
-        res.end = function (chunk: any) {
+        res.end = function(chunk: any) {
             if (chunk) chunks.push(chunk);
             if (this.statusCode >= 400) {
                 let body: string;
-                if (typeof chunks[0] ==='string') body = chunks.join();
+                if (typeof chunks[0] === 'string') body = chunks.join();
                 else body = Buffer.concat(chunks).toString('utf8');
                 console.log("ERROR: ", res.statusCode, req.path, body);
             }
@@ -124,26 +113,26 @@ next();
 
     console.log('Initializing routers...');
 
-    app.use('/api/games',game_router);
-    app.use('/api/users',user_router);
-    app.use('/api/reviews',review_router);
-    app.use('/api/lists',list_router);
-    app.use('/api/auth',auth_router);
-    app.use('/api/ping',ping_router);
-    app.use('/api/message',message_router);
-    app.use('/api/screenshots',screenshot_router);
-    app.use('/api/news',news_router);
-    app.use('/api/reports',report_router);
-    app.use('/api/tags',tag_router);
-    app.use('/api/api',api_router);
+    app.use('/api/games', game_router);
+    app.use('/api/users', user_router);
+    app.use('/api/reviews', review_router);
+    app.use('/api/lists', list_router);
+    app.use('/api/auth', auth_router);
+    app.use('/api/ping', ping_router);
+    app.use('/api/message', message_router);
+    app.use('/api/screenshots', screenshot_router);
+    app.use('/api/news', news_router);
+    app.use('/api/reports', report_router);
+    app.use('/api/tags', tag_router);
+    app.use('/api/api', api_router);
 
     console.log('Initializing object storage...');
 
     try {
         const minioClient = new Minio.Client(config.s3);
 
-        const bucketJustCreated = await new Promise((res,rej) => {
-            minioClient.bucketExists(config.s3_bucket,(err,exists)=>{
+        const bucketJustCreated = await new Promise((res, rej) => {
+            minioClient.bucketExists(config.s3_bucket, (err, exists) => {
                 if (err) return rej(err);
                 else if (exists) return res(false);
                 console.log(`Bucket ${config.s3_bucket} doesn't exist, intializing.`)
@@ -157,8 +146,8 @@ next();
 
         if (bucketJustCreated) {
             console.log(`Setting public read policy on ${config.s3_bucket}`)
-            await new Promise((res,rej)=>{
-                minioClient.setBucketPolicy(config.s3_bucket,`{
+            await new Promise((res, rej) => {
+                minioClient.setBucketPolicy(config.s3_bucket, `{
           "Version": "2012-10-17",
           "Id": "Public Access to Screenshots",
           "Statement": [
@@ -170,17 +159,17 @@ next();
               "Resource": "arn:aws:s3:::${config.s3_bucket}/*"
             }
           ]
-                    }`,(err) => {
-                        if (err) return rej(err);
-                        else res();
-                    }
+                    }`, (err) => {
+                    if (err) return rej(err);
+                    else res();
+                }
                 );
             });
         }
     } catch (e) {
         console.error("S3 initialization failed!");
         console.error(e);
-        process.exit(ExitCode.S3_INIT_FAIL);
+        return ExitCode.S3_INIT_FAIL;
     }
 
     console.log('Initializing swagger...');
@@ -210,21 +199,40 @@ next();
             scheme: "http",
             host: "localhost:4201",
         },
-        apis: [__dirname+'/*.ts'],
+        apis: [__dirname + '/*.ts'],
     };
 
     const specs = swaggerJsdoc(options);
     app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(specs));
 
     if (config.bcrypt_rounds < 10) {
-        console.log("WARNING!! bcrypt_rounds is less than 10. "+
+        console.log("WARNING!! bcrypt_rounds is less than 10. " +
             "Lower values mean faster hash attempts for password crackers!");
     }
 
     console.log('Starting app...');
 
-    app.listen(config.app_port,  () => {
+    const server = app.listen(config.app_port, () => {
         console.log(`Server started at localhost:${config.app_port}!`);
     });
 
-})();
+    while (server.listening) {
+        await sleep(100)
+    }
+
+
+    console.log("app done");
+
+    return ExitCode.SUCCESS;
+};
+
+
+function sleep(ms: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+(async () => {
+    process.exit(await main())
+})()
