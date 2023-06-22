b4b77d0950a77f5bf82d8b1683a54639b989fb64
Starz0r <starz0r@starz0r.com>
2023-06-22 01:05:12 -0500

    api-router: convert to controller with tsoa decorators

--- src/api-router.ts
+++ src/api-router.ts
@@ -1,63 +1,35 @@
-import express from 'express';
 import datastore from './datastore';
-import handle from './lib/express-async-catch';
-import { adminCheck } from './lib/auth-check';
-const app = express.Router();
-export default app;
+import { Controller, Get, Tags, Query, Route, SuccessResponse, Security } from "tsoa";
 
-/**
- * @swagger
- * 
- * /api/users:
- *   get:
- *     summary: API User List
- *     description: API User List
- *     tags: 
- *       - API
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: returns a list of api users
- *         content:
- *              application/json:
- *                      schema:
- *                              type: array
- *                              items:
- *                                      $ref: '#/components/schemas/ApiUser'
- * components:
- *      schemas:
- *              ApiUser:
- *                      type: object
- *                      properties:
- *                          id:
- *                              type: integer
- *                              format: int64
- *                          api_key:
- *                              type: string
- *                          description:
- *                              type: string
- *                          date_created:
- *                              type: string
- *                              format: date
- *                          origin:
- *                              type: string
- *                      required:
- *                              - id
- *                              - api_key
- *                              - date_created
- */
-app.route('/users').get(adminCheck(), handle(async (req, res, next) => {
-    var page = +req.query.page || 0;
-    var limit = +req.query.limit || 50;
-    const users = await datastore.getApiUsers({ page, limit });
-    users.forEach(u => {
-        delete u.email;
-        delete u.canReport;
-        delete u.canSubmit;
-        delete u.canReview;
-        delete u.canScreenshot;
-        delete u.banned;
-    });
-    return res.send(users);
-}));
+export interface ApiUser {
+    /** @isInt @format int64 */
+    id: number;
+    api_key: string;
+    description?: string;
+    /** @isDate @format date */
+    date_created: string;
+    origin?: string;
+}
+
+@Route("/api/users")
+@Tags("API")
+export class ApiController extends Controller {
+    /**
+     *  Retrieves a list of API users.
+     *  @summary API User List.
+    */
+    @Security("bearerAuth", ["admin"])
+    @SuccessResponse("200", "OK")
+    @Get()
+    public async getUsers(@Query() page: number = 0, @Query() limit: number = 50): Promise<ApiUser[]> {
+        const users = await datastore.getApiUsers({ page, limit });
+        users.forEach(u => {
+            delete u.email;
+            delete u.canReport;
+            delete u.canSubmit;
+            delete u.canScreenshot;
+            delete u.banned;
+        });
+        return users as ApiUser[]
+    }
+}
