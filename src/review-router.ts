b3044eb62dc616285ee2ef8c7614df3520799277
Starz0r <starz0r@starz0r.com>
2023-06-26 23:23:25 -0500

    review-router: convert to controller with tsoa decorators

--- src/review-router.ts
+++ src/review-router.ts
@@ -2,318 +2,153 @@ import datastore from './datastore';
 import express from 'express';
 import handle from './lib/express-async-catch';
 import { userCheck } from './lib/auth-check';
+import { Body, Controller, Delete, Get, Header, Patch, Path, Put, Query, Response, Route, Security, SuccessResponse, Tags } from 'tsoa';
+import { Review } from './model/Review';
 
 const app = express.Router();
 export default app;
 
-/**
- * @swagger
- * 
- * /reviews/{id}:
- *   get:
- *     summary: Get Review
- *     description: Get Review
- *     tags: 
- *       - Reviews
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The exact id of the review to return
- *     responses:
- *       200:
- *         description: Object describing the review
- *       400:
- *         description: Invalid review id
- *       404:
- *         description: Review not found
- */
-app.route('/:id').get(handle(async (req,res,next) => {
-  if (isNaN(+req.params.id)) return res.status(400).send({error:'id must be a number'});
+import Config from './model/config';
+let config: Config = require('./config/config.json');
 
-  var id = parseInt(req.params.id, 10);
-  const review = await datastore.getReview(id);
-  if (review == null) return res.sendStatus(404);
-  return res.send(review);
-}));
+import * as jwt from "jsonwebtoken";
+function extractBearerJWT(header_token: string): string | object {
+    if (!header_token.includes("Bearer ")) {
+        throw new Error("missing prefix")
+    }
+    const unverified_token = header_token.split(" ")[1];
 
-/**
- * @swagger
- * 
- * /reviews:
- *   get:
- *     summary: Get Reviews
- *     description: Get Reviews
- *     tags: 
- *       - Reviews
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: query
- *         name: page
- *         schema:
- *           type: integer
- *           minimum: 0
- *         description: The page of results to return (default 0)
- *       - in: query
- *         name: limit
- *         schema:
- *           type: integer
- *           minimum: 1
- *           maximum: 50
- *         description: The number of results per page (default 50)
- *     responses:
- *       200:
- *         description: List of matching reviews (or an empty array if no match)
- */
-app.route('/').get(handle(async (req,res,next) => {
-  //TODO: order by & dir
-  var page = +req.query.page || 0;
-  var limit = +req.query.limit || 50;
-  const rows = await datastore.getReviews({removed:false,page:page,limit:limit});
-  return res.send(rows);
-}));
+    try {
+        return jwt.verify(unverified_token, config.app_jwt_secret);
+    } catch (e) {
+        throw new Error(`invalid token: ${e}`)
+    }
+}
 
-/**
- * @swagger
- * 
- * /reviews/{id}:
- *   patch:
- *     summary: Update Review (User Only)
- *     description: Update Review (User Only)
- *     tags: 
- *       - Reviews
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The id of the review to update
- * 
- *     requestBody:
- *       description: Optional description in *Markdown*
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               rating: 
- *                 type: number
- *               difficulty: 
- *                 type: number
- *               comment: 
- *                 type: string
- *               removed: 
- *                 type: boolean
- * 
- *     responses:
- *       204:
- *         description: The review was updated
- *       400:
- *         description: Invalid review id
- *       401:
- *         description: Unauthorized (must log in to edit reviews)
- *       403:
- *         description: Insufficient privileges (must be an admin or the reviewer)
- */
-app.route('/:id').patch(userCheck(), handle(async (req,res,next) => {
-  const isAdmin = req.user.isAdmin;
+@Tags("Reviews")
+@Route("reviews")
+export class ReviewController extends Controller {
+    /**
+     * Get Review
+     * @summary Get Review
+     */
+    @SuccessResponse(200, "Review Details")
+    @Response<void>(400, "Not Found")
+    @Get("{id}")
+    public async getReview(@Path() id: number): Promise<Review> {
+        const review = await datastore.getReview(id);
+        if (review == null) return this.setStatus(404);
+        return review;
+    }
 
-  if (isNaN(+req.params.id)) return res.status(400).send({error:'id must be a number'});
-  const rid = req.params.id;
+    /**
+     * Get List of Reviews
+     * @summary Get List of Reviews
+     */
+    @SuccessResponse(200, "List of matching Reviews")
+    @Get()
+    public async getReviews(@Query() page?: number = 0, @Query() limit?: number = 50): Promise<Review[]> {
+        // TODO: order by & dir
+        // TODO: allow admins to toggle removed reviews
+        const rows = await datastore.getReviews({ removed: false, page: page, limit: limit });
+        return rows;
+    }
 
-  const ogReview = await datastore.getReview(+rid);
-  if (ogReview === null) return res.sendStatus(404);
+    /**
+     * Update Review (User Only)
+     * @summary Update Review (User Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(204, "Successfully Updated")
+    @Response<void>(403, "Bad Ownership")
+    @Response<void>(400, "Not Found")
+    @Patch("{id}")
+    public async patchReview(@Header("Authorization") authorization: string, @Path() id: number, @Body() requestBody: Review): Promise<void> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
 
-  const isReviewer = +(ogReview.user_id!) == +req.user.sub;
+        const isAdmin = user.isAdmin;
 
-  if (!isAdmin && !isReviewer) return res.sendStatus(403);
+        const ogReview = await datastore.getReview(+id);
+        if (ogReview === null) return this.setStatus(404);
 
-  const review = req.body;
-  review.id = rid;
+        const isReviewer = +(ogReview.user_id!) == +user.sub;
 
-  if (!isAdmin && !isReviewer) {
-    delete review.removed;
-  }
+        if (!isAdmin && !isReviewer) return this.setStatus(403);
 
-  await datastore.updateReview(review);
-  return res.sendStatus(204);
-}));
+        const review = requestBody;
+        review.id = +id;
 
-/**
- * @swagger
- * 
- * /reviews/{id}/likes/{userId}:
- *   put:
- *     summary: Like Review (User/Admin only)
- *     description: Indicates a user likes a review. Is idempotent - additional likes do nothing
- *     tags: 
- *       - Reviews
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The exact id of the review to like
- *       - in: path
- *         name: userId
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The id of the user performing the like
- *     responses:
- *       204:
- *         description: Like was accepted
- *       400:
- *         description: Invalid review id or user id (check the error in response)
- *       401:
- *         description: Unauthenticated (log in first)
- *       403:
- *         description: Insufficient priviliges (must be an admin, or the user indicated in the url)
- *       404:
- *         description: Review not found
- */
-app.route('/:id/likes/:userId').put(userCheck(), handle(async (req,res,next) => {
-  if (isNaN(+req.params.id)) 
-    return res.status(400).send({error:'id must be a number'});
-  const rid = req.params.id;
+        if (!isAdmin && !isReviewer) {
+            delete review.removed;
+        }
 
-  if (isNaN(+req.params.userId)) 
-    return res.status(400).send({error:'userId must be a number'});
-  const uid = parseInt(req.params.userId, 10);
-  
-  if (req.user.sub != uid) return res.sendStatus(403);
+        await datastore.updateReview(review);
+        return this.setStatus(204);
+    }
 
-  const ogReview = await datastore.getReview(+rid);
-  if (ogReview === null) return res.sendStatus(404);
+    /**
+     * Like Review (User/Admin Only)
+     * @summary Like Review (User/Admin Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(204, "Like Accepted")
+    @Response<void>(403, "Bad Ownership")
+    @Response<void>(404, "Not Found")
+    @Put("{id}/likes/{userId}")
+    public async putReviewLike(@Header("Authorization") authorization: string, @Path() id: number, @Path() userId: number): Promise<void> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
 
-  await datastore.addLikeToReview(+rid,uid);
-  return res.sendStatus(204);
-}));
+        if (user.sub != userId) return this.setStatus(403);
 
-/**
- * @swagger
- * 
- * /reviews/{id}/likes/{userId}:
- *   put:
- *     summary: Unlike Review (User/Admin only)
- *     description: Removes the user's like from a review. Is idempotent - additional unlikes do nothing
- *     tags: 
- *       - Reviews
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The exact id of the review to unlike
- *       - in: path
- *         name: userId
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The id of the user performing the unlike
- *     responses:
- *       204:
- *         description: Unlike was accepted
- *       400:
- *         description: Invalid review id or user id (check the error in response)
- *       401:
- *         description: Unauthenticated (log in first)
- *       403:
- *         description: Insufficient priviliges (must be an admin, or the user indicated in the url)
- *       404:
- *         description: Review not found
- */
-app.route('/:id/likes/:userId').delete(userCheck(), handle(async (req,res,next) => {
-  if (isNaN(+req.params.id)) 
-    return res.status(400).send({error:'id must be a number'});
-  const rid = req.params.id;
+        const ogReview = await datastore.getReview(+id);
+        if (ogReview === null) return this.setStatus(404);
 
-  if (isNaN(+req.params.userId)) 
-    return res.status(400).send({error:'userId must be a number'});
-  const uid = parseInt(req.params.userId, 10);
-  
-  if (req.user.sub != uid) return res.sendStatus(403);
+        await datastore.addLikeToReview(+id, userId);
+        return this.setStatus(204);
+    }
 
-  const ogReview = await datastore.getReview(+rid);
-  if (ogReview === null) return res.sendStatus(404);
+    /**
+     * Unlike Review (User/Admin Only)
+     * @summary Unlike Review (User/Admin Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(204, "Like Accepted")
+    @Response<void>(403, "Bad Ownership")
+    @Response<void>(404, "Not Found")
+    @Delete("{id}/likes/{userId}")
+    public async deleteReviewLike(@Header("Authorization") authorization: string, @Path() id: number, @Path() userId: number): Promise<void> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
 
-  await datastore.removeLikeFromReview(+rid,uid);
-  return res.sendStatus(204);
-}));
+        if (user.sub != userId) return this.setStatus(403);
 
-/**
- * @swagger
- * 
- * /reviews/{id}/likes/{userId}:
- *   get:
- *     summary: Returns whether user likes review (User/Admin only)
- *     description: Returns whether user likes review (User/Admin only)
- *     tags: 
- *       - Reviews
- *     produces:
- *       - application/json
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The exact id of the review
- *       - in: path
- *         name: userId
- *         schema:
- *           type: integer
- *           minimum: 1
- *         required: true
- *         description: The id of the user to check
- *     responses:
- *       200:
- *         description: User's like status
- *       400:
- *         description: Invalid review id or user id (check the error in response)
- *       401:
- *         description: Unauthenticated (log in first)
- *       403:
- *         description: Insufficient priviliges (must be an admin, or the user indicated in the url)
- *       404:
- *         description: Review not found
- */
-app.route('/:id/likes/:userId').get(userCheck(), handle(async (req,res,next) => {
-  if (isNaN(+req.params.id)) 
-    return res.status(400).send({error:'id must be a number'});
-  const rid = req.params.id;
+        const ogReview = await datastore.getReview(+id);
+        if (ogReview === null) return this.setStatus(404);
 
-  if (isNaN(+req.params.userId)) 
-    return res.status(400).send({error:'userId must be a number'});
-  const uid = parseInt(req.params.userId, 10);
-  
-  if (req.user.sub != uid) return res.sendStatus(403);
+        await datastore.removeLikeFromReview(+id, userId);
+        return this.setStatus(204);
+    }
+    /**
+     * Returns whether a user likes a specific review. (User/Admin Only)
+     * @summary Like Check for User (User/Admin Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(204, "Like Accepted")
+    @Response<void>(403, "Bad Ownership")
+    @Response<void>(404, "Not Found")
+    @Get("{id}/likes/{userId}")
+    public async deleteReviewLike(@Header("Authorization") authorization: string, @Path() id: number, @Path() userId: number): Promise<boolean> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
 
-  const ogReview = await datastore.getReview(+rid);
-  if (ogReview === null) return res.sendStatus(404);
+        if (user.sub != userId) return this.setStatus(403);
 
-  const liked = await datastore.isLiked(+rid,uid);
-  return res.send({liked});
-}));
+        const ogReview = await datastore.getReview(+id);
+        if (ogReview === null) return this.setStatus(404);
+
+        const liked = await datastore.isLiked(+id, userId);
+        return liked;
+    }
+}
