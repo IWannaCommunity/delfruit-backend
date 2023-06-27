7f532d9b1c0c3036e8c4e6765f33e758ce9241df
Starz0r <starz0r@starz0r.com>
2023-06-26 19:35:34 -0500

    message-router: convert to controller with tsoa decorators

--- src/message-router.ts
+++ src/message-router.ts
@@ -6,81 +6,118 @@ import { Message } from './model/Message';
 import { MessageQueryParams } from './model/MessageQueryParams';
 import handle from './lib/express-async-catch';
 import { userCheck } from './lib/auth-check';
+import { Body, Controller, Get, Header, Path, Post, Route, Security, SuccessResponse, Tags } from 'tsoa';
 
 const app = express.Router();
 export default app;
 
-app.route('/inbox').get(userCheck(), handle(async (req,res,next) => {
-  let parms = {} as MessageQueryParams;
-  parms.userToId = req.user.sub; //force to for inbox
+function extractBearerJWT(header_token: string): string | object {
+    if (!header_token.includes("Bearer ")) {
+        throw new Error("missing prefix")
+    }
+    const unverified_token = header_token.split(" ")[1];
 
-  const whereList = new WhereList();
-  whereList.add("user_to_id",parms.userToId);
+    try {
+        return jwt.verify(unverified_token, config.app_jwt_secret);
+    } catch (e) {
+        throw new Error(`invalid token: ${e}`)
+    }
+}
 
-  const database = new Database();
-  try {
-    const messages = await database.query(`
+@Tags("Messages")
+@Route("message")
+export class MessageController extends Controller {
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(200, "List of Messages")
+    @Get("/inbox")
+    public async getMessagesFromInbox(@Header("Authorization") authorization: string): Promise<Message[]> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
+
+        let parms = {} as MessageQueryParams;
+        parms.userToId = user.sub; //force to for inbox
+
+        const whereList = new WhereList();
+        whereList.add("user_to_id", parms.userToId);
+
+        const database = new Database();
+        try {
+            const messages = await database.query(`
       SELECT *, thread_id as threadId
       FROM Message 
-      ${whereList.getClause()}`,whereList.getParams());
-    return res.send(messages);
-  } finally {
-    database.close();
-  }
-}));
-
-app.route('/thread/:id').get(userCheck(), handle(async (req,res,next) => {
-  let parms = {} as MessageQueryParams;
-  parms.threadId = +req.params.id;
-
-  const whereList = new WhereList();
-  whereList.add("thread_id",parms.threadId);
-  //don't allow viewing a thread if you're not one of the participants
-  whereList.addPhrase("user_to_id = ? OR user_from_id = ?",req.user.sub,req.user.sub);
-
-  const database = new Database();
-  try {
-    const messages = await database.query(`
-      SELECT *
-      FROM Message 
-      ${whereList.getClause()}`,whereList.getParams());
-    return res.send(messages);
-  } finally {
-    database.close();
-  }
-}));
-  
-app.route('/').post(userCheck(), handle(async (req,res,next) => {
-  const msg = req.body as Message;
-
-  const insertList = new InsertList();
-  insertList.add("user_from_id",req.user.sub);
-  insertList.add("user_to_id",msg.userToId);
-  insertList.add("subject",msg.subject);
-  insertList.add("body",msg.body);
-  insertList.add("reply_to_id",msg.replyToId);
-    
-  const database = new Database();
-  try {
-    let result = await database.execute(`
-      INSERT INTO Message ${insertList.getClause()}`,insertList.getParams());
-    if (!msg.replyToId) { //new thread
-      result = await database.execute(`
-        UPDATE Message SET thread_id = id WHERE id = ?`,result.insertId);
-      if (result.affectedRows != 1) throw 'Wonky database error!';
-    } else {
-      let replyTo = await database.query(`
-        SELECT thread_id FROM Message WHERE id = ?`,result.replyToId);
-      if (replyTo.length != 1) {
-        
-      }
-      result = await database.execute(`
-        UPDATE Message SET thread_id = ? WHERE id = ?`,[replyTo[0].thread_id, result.insertId]);
-      if (result.affectedRows != 1) throw 'Wonky database error!';
+      ${whereList.getClause()}`, whereList.getParams());
+            return messages;
+        } finally {
+            database.close();
+        }
     }
 
-    return res.sendStatus(204);
-  } finally {
-    database.close();
-  }
-}));
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(200, "List of Messages from thread")
+    @Get("thread/{id}")
+    public async getMessagesFromThread(@Header("Authorization") authorization: string, @Path() id: number): Promise<Message[]> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
+
+        let parms = {} as MessageQueryParams;
+        parms.threadId = +id;
+
+        const whereList = new WhereList();
+        whereList.add("thread_id", parms.threadId);
+        //don't allow viewing a thread if you're not one of the participants
+        whereList.addPhrase("user_to_id = ? OR user_from_id = ?", user.sub, user.sub);
+
+        const database = new Database();
+        try {
+            const messages = await database.query(`
+      SELECT *
+      FROM Message 
+      ${whereList.getClause()}`, whereList.getParams());
+            return messages;
+        } finally {
+            database.close();
+        }
+
+    }
+
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(201, "Sent Message")
+    @Post()
+    public async postMessage(@Header("Authorization") authorization: string, @Body() requestBody: Message): Promise<void> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
+
+        const insertList = new InsertList();
+        insertList.add("user_from_id", user.sub);
+        insertList.add("user_to_id", requestBody.userToId);
+        insertList.add("subject", requestBody.subject);
+        insertList.add("body", requestBody.body);
+        insertList.add("reply_to_id", requestBody.replyToId);
+
+        const database = new Database();
+        try {
+            let result = await database.execute(`
+      INSERT INTO Message ${insertList.getClause()}`, insertList.getParams());
+            if (!requestBody.replyToId) { //new thread
+                result = await database.execute(`
+        UPDATE Message SET thread_id = id WHERE id = ?`, result.insertId);
+                if (result.affectedRows != 1) throw 'Wonky database error!';
+            } else {
+                let replyTo = await database.query(`
+        SELECT thread_id FROM Message WHERE id = ?`, result.replyToId);
+                if (replyTo.length != 1) {
+
+                }
+                result = await database.execute(`
+        UPDATE Message SET thread_id = ? WHERE id = ?`, [replyTo[0].thread_id, result.insertId]);
+                if (result.affectedRows != 1) throw 'Wonky database error!';
+            }
+
+            this.setStatus(201);
+            return;
+        } finally {
+            database.close();
+        }
+
+    }
+}
