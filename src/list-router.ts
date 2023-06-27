29474e7789852ce5588f82192668f2d610512215
Starz0r <starz0r@starz0r.com>
2023-06-26 21:28:50 -0500

    list-router: convert to controller with tsoa decorators

--- src/list-router.ts
+++ src/list-router.ts
@@ -2,45 +2,114 @@ import express from 'express';
 import datastore from './datastore';
 import handle from './lib/express-async-catch';
 import { userCheck } from './lib/auth-check';
+import { Body, Controller, Delete, Get, Header, Patch, Path, Post, Put, Response, Route, Security, SuccessResponse, Tags } from 'tsoa';
+import { List } from './model/List';
 
 const app = express.Router();
 export default app;
 
-app.route('/').post(userCheck(), handle(async (req, res, next) => {
-  const list = await datastore.addList(req.body, req.user.sub);
-  res.send(list);
-}));
+import Config from './model/config';
+let config: Config = require('./config/config.json');
 
-//update list
-app.route('/:listId').patch(handle(async (req, res, next) => {
-}));
+import * as jwt from "jsonwebtoken";
+function extractBearerJWT(header_token: string): string | object {
+    if (!header_token.includes("Bearer ")) {
+        throw new Error("missing prefix")
+    }
+    const unverified_token = header_token.split(" ")[1];
 
-//get list
-app.route('/:listId').get(handle(async (req, res, next) => {
-}));
+    try {
+        return jwt.verify(unverified_token, config.app_jwt_secret);
+    } catch (e) {
+        throw new Error(`invalid token: ${e}`)
+    }
+}
 
-//get list games
-app.route('/:listId/games').get(handle(async (req, res, next) => {
-}));
+@Tags("Lists")
+@Route("lists")
+export class ListController extends Controller {
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(201, "Created")
+    @Post()
+    public async postList(@Header("Authorization") authorization: string, @Body() requestBody: List): Promise<List> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
 
-//replace games in list
-app.route('/:listId/games').put(userCheck(), handle(async (req, res, next) => {
-  const gid = req.body.gameId;
-  const lid = parseInt(req.params.listId, 10);
+        const list = await datastore.addList(requestBody, user.sub);
+        return list;
+    }
 
-  const list = await datastore.getList(lid);
-  if (!list) return res.sendStatus(404);
+    /**
+     * Update List (User/Admin Only)
+     * @summary Update List (User/Admin Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(200, "Updated Successfully")
+    @Patch("{id}")
+    public async patchList(@Header("Authorization") authorization: string, @Path() id: number): Promise<void> {
+        // TODO: currently unimplemented
+    }
 
-  if (list.userId !== req.user.sub) return res.sendStatus(403);
+    /**
+     * Get List
+     * @summary Get List
+     */
+    @SuccessResponse(200, "Found")
+    @Get("{id}")
+    public async getList(@Header("Authorization") authorization: string, @Path() id: number): Promise<void> {
+        // TODO: currently unimplemented
+    }
 
-  const games = await datastore.getListGames(lid);
-  if (games.includes(gid)) return res.sendStatus(204);
+    /**
+     * Get List Contents
+     * @summary Get List Contents
+     */
+    @SuccessResponse(200, "Found")
+    @Get("{id}/games")
+    public async getListGames(@Header("Authorization") authorization: string, @Path() id: number): Promise<void> {
+        // TODO: currently unimplemented
+    }
 
-  await datastore.addGameToList(lid,gid);
-  return res.sendStatus(204);
-}));
+    /**
+     * Replace Games in List (User/Admin Only)
+     * @summary Replace Games in List (User/Admin Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(204, "Updated Successfully")
+    @Response<void>(403, "Bad Ownership")
+    @Response<void>(404, "Not Found")
+    @Put("{id}/games")
+    public async putList(@Header("Authorization") authorization: string, @Path() id: number, @Body() requestBody: any): Promise<void> {
+        // NOTE: auth guard should make the error condition unreachable
+        const user = extractBearerJWT(authorization);
 
-//delete list
-app.route('/:listId').delete(userCheck(), handle(async (req, res, next) => {
-  //don't allow deleting lists 1,2 - favorites and clears
-}));
+        const list = await datastore.getList(id);
+        if (!list) return this.setStatus(404);
+
+        if (list.userId !== user.sub) return this.setStatus(403);
+
+        const games = await datastore.getListGames(id);
+        if (games.includes(requestBody.gameId)) return this.setStatus(204);
+
+        await datastore.addGameToList(id, requestBody.gameId);
+        return this.setStatus(204);
+    }
+
+    /**
+     * Delete List (User/Admin Only)
+     * @summary Delete List (User/Admin Only)
+     */
+    @Security("bearerAuth", ["user"])
+    @SuccessResponse(200, "Deleted")
+    @Response<void>(403, "Bad Ownership")
+    @Response<void>(404, "Not Found")
+    @Delete("{id}")
+    public async deleteList(@Header("Authorization") authorization: string, @Path() id: number): Promise<void> {
+        // don't allow deleting lists 1,2 - favorites and clears
+        // TODO: unimplemented
+    }
+}
+
+
+
+
