8587d698a50aa215621db5b12876eb6fb579728a
Starz0r <starz0r@starz0r.com>
2025-08-18 21:13:40 -0400

    composite-router: APIs for getting ratings and everything with a game

---
+++ src/composite-router.ts
@@ -0,0 +1,181 @@
+import datastore from "./datastore";
+import { Controller, Get, Tags, Query, Route, SuccessResponse, Security, Header, Response, Path } from "tsoa";
+import { GetGamesParms } from "./model/GetGamesParms";
+import whitelist from "./lib/whitelist";
+import { Game } from "./model/Game";
+import { Review } from "./model/Review";
+import { Screenshot } from "./model/Screenshot";
+
+function extractBearerJWT(header_token: string): string | object {
+    if (!header_token.includes("Bearer ")) {
+        throw new Error("missing prefix");
+    }
+    const unverified_token = header_token.split(" ")[1];
+
+    try {
+        return jwt.verify(unverified_token, config.app_jwt_secret);
+    } catch (e) {
+        throw new Error(`invalid token: ${e}`);
+    }
+}
+
+interface APIError {
+    error: string;
+}
+
+interface GameExt extends Game {
+    ratings: {
+        rating: number;
+        difficulty: number;
+    };
+    reviews: Review[];
+    tags: string[];
+    screenshots: Screenshot[];
+}
+
+@Route("/games/composite")
+@Tags("Composite", "Games")
+export class CompositeController extends Controller {
+    /**
+     * Get Game
+     * @summary Get Game
+     */
+    @SuccessResponse(200, "Found Game Details")
+    @Response<APIError>(400, "Unparsable ID")
+    @Response(404, "Not Found")
+    @Get("{id}/all")
+    public async getGameCompositeAll(@Path() id: string): Promise<GameExt> {
+        // TODO: this could be simplified by making id optional, and where it isn't provided, assume "random"
+        let game: GameExt;
+        if (id === "random") {
+            game = await datastore.getRandomGame();
+        } else if (!isNaN(+id)) {
+            const game_id = parseInt(id, 10);
+            game = await datastore.getGame(game_id);
+        } else {
+            this.setStatus(400);
+            return { error: "id must be a number" };
+        }
+
+        if (!game) {
+            this.setStatus(404);
+            return;
+        }
+
+        //get owner review
+        if (game.ownerId) {
+            const ownerReviews = await datastore.getReviews({
+                game_id: game.id,
+                user_id: +game.ownerId,
+                includeOwnerReview: true,
+                removed: false,
+            });
+            if (ownerReviews.length == 1) {
+                game.ownerBio = ownerReviews[0];
+            }
+        }
+
+        const [ratings, reviews, tags, screenshots] = await Promise.all([
+            datastore.getRatings(game.id),
+            datastore.getReviews({ game_id: game.id }),
+            datastore.getTagsForGame(game.id, void 0),
+            datastore.getScreenshots({ gameId: game.id }),
+        ]);
+        game.ratings = ratings;
+        game.reviews = reviews;
+        game.tags = tags;
+        game.screenshots = screenshots;
+
+        return game;
+    }
+
+    /**
+     * Game List
+     * @summary Game List
+     */
+    @SuccessResponse(200, "List of Games matching filters")
+    @Response<APIError>(400, "Invalid Parameters")
+    @Get("rating")
+    public async getGamesWithRatings(
+        @Header("Authorization") authorization?: string,
+        @Query() id?: number,
+        @Query() removed?: boolean,
+        @Query() name?: string,
+        @Query() tags?: string,
+        @Query() author?: string,
+        @Query() ownerUserId?: number,
+        @Query() hasDownload?: boolean,
+        @Query() createdFrom?: Date,
+        @Query() createdTo?: Date,
+        @Query() clearedByUserId?: number,
+        @Query() reviewedByUserId?: number,
+        @Query() ratingFrom?: number,
+        @Query() ratingTo?: number,
+        @Query() difficultyFrom?: number,
+        @Query() difficultyTo?: number,
+        @Query() page?: number,
+        @Query() limit?: number,
+        @Query() order_col?: string,
+        @Query() order_dir?: string,
+    ): Promise<GameExt[]> {
+        // TODO: do type restrictions in method definition
+        order_col = whitelist(order_col, ["name", "date_created", "rating", "difficulty"], "name");
+        order_dir = whitelist(order_dir, ["asc", "desc"], "asc") as "asc" | "desc";
+        let isAdmin = false;
+        try {
+            const user = extractBearerJWT(authorization);
+            isAdmin = user.isAdmin;
+        } catch (_) {
+            console.warn("user provided authorization, but it was invalid");
+        }
+
+        const params: GetGamesParms = {
+            page: +(page || 0),
+            limit: +(limit || 50),
+            orderCol: order_col,
+            orderDir: order_dir,
+        };
+        if (!isAdmin) params.removed = false;
+
+        //params.q = req.query.q; // QUEST: what the hell is Q?
+        params.id = id;
+        params.removed = false; // QUEST: shouldn't this just use the removed query parameter?
+        params.name = name;
+
+        if (tags) {
+            try {
+                params.tags = <string[]>JSON.parse(tags);
+                params.tags.forEach((s, i) => {
+                    if (+s === NaN) throw "tag #" + i + " was not a number -> " + s;
+                });
+            } catch (e) {
+                this.setStatus(400);
+                return { error: "tags must be an array of numbers" };
+            }
+        }
+        params.author = author;
+        params.hasDownload = hasDownload;
+        params.createdFrom = createdFrom?.toString();
+        params.createdTo = createdTo?.toString();
+        params.clearedByUserId = clearedByUserId;
+        params.reviewedByUserId = reviewedByUserId;
+
+        params.ratingFrom = ratingFrom;
+        params.ratingTo = ratingTo;
+        params.difficultyFrom = difficultyFrom;
+        params.difficultyTo = difficultyTo;
+        params.ownerUserId = ownerUserId;
+
+        const rows: GameExt[] = await datastore.getGames(params);
+        if (!params.page) {
+            const total = await datastore.countGames(params);
+            this.setHeader("total-count", total);
+        }
+
+        for (const row of rows) {
+            row.ratings = await datastore.getRatings(row.id);
+        }
+
+        return rows;
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,41 @@
-import express, { ErrorRequestHandler, urlencoded } from 'express';
-import bodyParser from 'body-parser';
-import uuid from 'uuid/v4';
-import cors from 'cors';
+import express, { ErrorRequestHandler, urlencoded } from "express";
+import bodyParser from "body-parser";
+import uuid from "uuid/v4";
+import cors from "cors";
 
-import jwt_middleware from 'express-jwt';
-import game_router from './game-router';
-import user_router from './user-router';
-import review_router from './review-router';
-import list_router from './list-router';
-import auth_router from './auth-router';
-import ping_router from './ping-router';
-import message_router from './message-router';
-import screenshot_router from './screenshot-router';
-import news_router from './news-router';
-import report_router from './report-router';
-import tag_router from './tag-router';
-import api_router from './api-router';
-import { Database } from './database';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import * as Minio from 'minio';
-import { refreshToken } from './lib/auth-check';
-import Config from './model/config';
+import jwt_middleware from "express-jwt";
+import game_router from "./game-router";
+import user_router from "./user-router";
+import review_router from "./review-router";
+import list_router from "./list-router";
+import auth_router from "./auth-router";
+import ping_router from "./ping-router";
+import message_router from "./message-router";
+import screenshot_router from "./screenshot-router";
+import news_router from "./news-router";
+import report_router from "./report-router";
+import tag_router from "./tag-router";
+import api_router from "./api-router";
+import composite_router from "./composite-router";
+import { Database } from "./database";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import * as Minio from "minio";
+import { refreshToken } from "./lib/auth-check";
+import Config from "./model/config";
 import { RegisterRoutes } from "../build/routes";
-let config: Config = require('./config/config.json');
+let config: Config = require("./config/config.json");
 
 import fs from "fs";
 import path from "path";
 
 /** Exit codes for fatal errors. */
 enum ExitCode {
-    SUCCESS = 0,
-    /** Database initialization failed. */
-    DB_INIT_FAIL = 1,
-    /** S3 Object storage initialization failed. */
-    S3_INIT_FAIL = 2
+	SUCCESS = 0,
+	/** Database initialization failed. */
+	DB_INIT_FAIL = 1,
+	/** S3 Object storage initialization failed. */
+	S3_INIT_FAIL = 2,
 }
 
 /**
@@ -48,113 +49,118 @@ enum ExitCode {
  *                                      type: string
  *                      required:
  *                              - error
-*/
+ */
 
 async function main(): Promise<number> {
+	console.log("Welcome to delfruit server 2.0!");
 
-    console.log('Welcome to delfruit server 2.0!');
+	// TODO: database initialization step, currently assumed database is already initialized
 
-    // TODO: database initialization step, currently assumed database is already initialized 
+	console.log("Initializing express...");
 
-    console.log('Initializing express...');
+	const app = express();
 
-    const app = express();
+	app.use(cors());
 
-    app.use(cors())
+	app.use(bodyParser.json({ type: "application/json" }));
 
-    app.use(bodyParser.json({ type: "application/json" }));
+	console.log("Initializing jwt middleware...");
 
-    console.log('Initializing jwt middleware...');
+	app.use(
+		jwt_middleware({
+			secret: config.app_jwt_secret,
+			credentialsRequired: false,
+		}),
+	);
+	app.use(refreshToken());
 
-    app.use(jwt_middleware({
-        secret: config.app_jwt_secret,
-        credentialsRequired: false
-    }));
-    app.use(refreshToken());
+	console.log("Initializing role middleware...");
 
-    console.log('Initializing role middleware...');
+	app.use((req, res, next) => {
+		if (req.user) {
+			req.user.roles = ["game_update"];
+		}
+		next();
+	});
 
-    app.use((req, res, next) => {
-        if (req.user) {
-            req.user.roles = ['game_update'];
-        }
-        next();
-    });
+	const e: ErrorRequestHandler = (err, req, res, next) => {
+		if (err && err.name && err.name === "UnauthorizedError") {
+			//invalid token, jwt middleware returns more info in the err
+			//but we don't want the client to see
+			// message: 'jwt malformed',
+			// code: 'invalid_token',
+			return res.sendStatus(401);
+		}
 
-    const e: ErrorRequestHandler = (err, req, res, next) => {
-        if (err && err.name && err.name === 'UnauthorizedError') {
-            //invalid token, jwt middleware returns more info in the err
-            //but we don't want the client to see
-            // message: 'jwt malformed',
-            // code: 'invalid_token',
-            return res.sendStatus(401);
-        }
+		const id = uuid();
+		console.log(`severe error: id ${id}`);
+		console.log(err);
+		res.status(500).send({
+			error: "Internal Server Error",
+			id: id,
+		});
+	};
+	app.use(e);
 
-        const id = uuid();
-        console.log(`severe error: id ${id}`);
-        console.log(err);
-        res.status(500).send({
-            error: "Internal Server Error",
-            id: id
-        });
-    }
-    app.use(e);
+	// DEBUG ERROR LOGGING
+	app.use((req, res, next) => {
+		var oldWrite = res.write,
+			oldEnd = res.end;
 
-    // DEBUG ERROR LOGGING
-    app.use((req, res, next) => {
-        var oldWrite = res.write,
-            oldEnd = res.end;
+		var chunks: any[] = [];
 
-        var chunks: any[] = [];
+		res.write = function (chunk: any) {
+			chunks.push(chunk);
+			// @ts-ignore
+			return oldWrite.apply(res, arguments);
+		};
 
-        res.write = function(chunk: any) {
-            chunks.push(chunk);
-            // @ts-ignore
-            return oldWrite.apply(res, arguments);
-        };
+		res.end = function (chunk: any) {
+			if (chunk) chunks.push(chunk);
+			if (this.statusCode >= 400) {
+				let body: string;
+				if (typeof chunks[0] === "string") body = chunks.join();
+				else body = Buffer.concat(chunks).toString("utf8");
+				console.log("ERROR: ", res.statusCode, req.path, body);
+			}
+			// @ts-ignore
+			oldEnd.apply(res, arguments);
+		};
 
-        res.end = function(chunk: any) {
-            if (chunk) chunks.push(chunk);
-            if (this.statusCode >= 400) {
-                let body: string;
-                if (typeof chunks[0] === 'string') body = chunks.join();
-                else body = Buffer.concat(chunks).toString('utf8');
-                console.log("ERROR: ", res.statusCode, req.path, body);
-            }
-            // @ts-ignore
-            oldEnd.apply(res, arguments);
-        };
+		next();
+	});
 
-        next();
-    });
+	console.log("Initializing routers...");
 
-    console.log('Initializing routers...');
+	app.use(urlencoded({ extended: true }));
+	RegisterRoutes(app);
 
-    app.use(urlencoded({ extended: true, }));
-    RegisterRoutes(app);
+	console.log("Initializing object storage...");
 
-    console.log('Initializing object storage...');
+	try {
+		const minioClient = new Minio.Client(config.s3);
 
-    try {
-        const minioClient = new Minio.Client(config.s3);
+		const bucketJustCreated = await new Promise((res, rej) => {
+			minioClient.bucketExists(config.s3_bucket, (err, exists) => {
+				if (err) return rej(err);
+				else if (exists) return res(false);
+				console.log(`Bucket ${config.s3_bucket} doesn't exist, intializing.`);
+				minioClient.makeBucket(config.s3_bucket, config.s3_region, (err) => {
+					if (err) return rej(err);
+					console.log(
+						`Bucket ${config.s3_bucket} created successfully in ${config.s3_region}.`,
+					);
+					res(true);
+				});
+			});
+		});
 
-        const bucketJustCreated = await new Promise((res, rej) => {
-            minioClient.bucketExists(config.s3_bucket, (err, exists) => {
-                if (err) return rej(err);
-                else if (exists) return res(false);
-                console.log(`Bucket ${config.s3_bucket} doesn't exist, intializing.`)
-                minioClient.makeBucket(config.s3_bucket, config.s3_region, (err) => {
-                    if (err) return rej(err);
-                    console.log(`Bucket ${config.s3_bucket} created successfully in ${config.s3_region}.`)
-                    res(true);
-                });
-            });
-        });
-
-        if (bucketJustCreated) {
-            console.log(`Setting public read policy on ${config.s3_bucket}`)
-            await new Promise((res, rej) => {
-                minioClient.setBucketPolicy(config.s3_bucket, `{
+		if (bucketJustCreated) {
+			console.log(`Setting public read policy on ${config.s3_bucket}`);
+			await new Promise((res, rej) => {
+				minioClient.setBucketPolicy(
+					config.s3_bucket,
+					`{
           "Version": "2012-10-17",
           "Id": "Public Access to Screenshots",
           "Statement": [
@@ -166,57 +172,64 @@ async function main(): Promise<number> {
               "Resource": "arn:aws:s3:::${config.s3_bucket}/*"
             }
           ]
-                    }`, (err) => {
-                    if (err) return rej(err);
-                    else res();
-                }
-                );
-            });
-        }
-    } catch (e) {
-        console.error("S3 initialization failed!");
-        console.error(e);
-        return ExitCode.S3_INIT_FAIL;
-    }
+                    }`,
+					(err) => {
+						if (err) return rej(err);
+						else res();
+					},
+				);
+			});
+		}
+	} catch (e) {
+		console.error("S3 initialization failed!");
+		console.error(e);
+		return ExitCode.S3_INIT_FAIL;
+	}
 
-    console.log('Initializing swagger...');
+	console.log("Initializing swagger...");
 
-    fs.readFile(path.join(__dirname, "../build/swagger.json"), { encoding: "utf-8" }, (err, data) => {
-        if (err) {
-            return console.error(`OpenAPI definition could not be opened or found: ${err}`);
-        }
-        const specs = JSON.parse(data);
-        app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
-    });
+	fs.readFile(
+		path.join(__dirname, "../build/swagger.json"),
+		{ encoding: "utf-8" },
+		(err, data) => {
+			if (err) {
+				return console.error(
+					`OpenAPI definition could not be opened or found: ${err}`,
+				);
+			}
+			const specs = JSON.parse(data);
+			app.use("/", swaggerUi.serve, swaggerUi.setup(specs));
+		},
+	);
 
-    if (config.bcrypt_rounds < 10) {
-        console.log("WARNING!! bcrypt_rounds is less than 10. " +
-            "Lower values mean faster hash attempts for password crackers!");
-    }
+	if (config.bcrypt_rounds < 10) {
+		console.log(
+			"WARNING!! bcrypt_rounds is less than 10. " +
+				"Lower values mean faster hash attempts for password crackers!",
+		);
+	}
 
-    console.log('Starting app...');
+	console.log("Starting app...");
 
-    const server = app.listen(config.app_port, () => {
-        console.log(`Server started at localhost:${config.app_port}!`);
-    });
+	const server = app.listen(config.app_port, () => {
+		console.log(`Server started at localhost:${config.app_port}!`);
+	});
 
-    while (server.listening) {
-        await sleep(100)
-    }
+	while (server.listening) {
+		await sleep(100);
+	}
 
+	console.log("app done");
 
-    console.log("app done");
-
-    return ExitCode.SUCCESS;
-};
-
+	return ExitCode.SUCCESS;
+}
 
 function sleep(ms: number) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
 }
 
 (async () => {
-    process.exit(await main())
-})()
+	process.exit(await main());
+})();
