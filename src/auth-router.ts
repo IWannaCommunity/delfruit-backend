27ca4894899fd6730ee6f405d52e33f99724b153
Starz0r <starz0r@starz0r.com>
2025-09-01 02:40:45 -0400

    auth-router: export AuthResponse interface

--- src/auth-router.ts
+++ src/auth-router.ts
@@ -1,60 +1,60 @@
-import express from 'express';
-import { Database } from './database';
-import AuthModule from './lib/auth';
-import datastore from './datastore';
-import moment = require('moment');
-import crypto from 'crypto';
-import nodemailer from 'nodemailer';
+import express from "express";
+import { Database } from "./database";
+import AuthModule from "./lib/auth";
+import datastore from "./datastore";
+import moment = require("moment");
+import crypto from "crypto";
+import nodemailer from "nodemailer";
 import * as jwt from "jsonwebtoken";
-import util from 'util';
-import axios from 'axios';
-import Config from './model/config';
-import { Body, Controller, Get, Header, Post, Response, Route, SuccessResponse, Tags } from 'tsoa';
-let config: Config = require('./config/config.json');
+import util from "util";
+import axios from "axios";
+import Config from "./model/config";
+import { Body, Controller, Get, Header, Post, Response, Route, SuccessResponse, Tags } from "tsoa";
+let config: Config = require("./config/config.json");
 
 const app = express.Router();
 const auth = new AuthModule();
 export default app;
 
-
-interface AuthResponse {
+export interface AuthResponse {
     /** @isInt @format int64 */
-    id: number,
-    name: string,
-    phash2: string, // TODO: REMOVE THIS!!!!
+    id: number;
+    name: string;
+    phash2: string; // TODO: REMOVE THIS!!!!
     /** @isDate @format date */
-    dateCreated: string,
-    twitchLink?: string,
-    youtubeLink?: string,
-    nicoLink?: string,
-    twitterLink?: string,
-    bio?: string,
-    isAdmin: boolean,
-    email?: string,
-    banned: boolean,
+    dateCreated: string;
+    twitchLink?: string;
+    youtubeLink?: string;
+    nicoLink?: string;
+    twitterLink?: string;
+    bio?: string;
+    isAdmin: boolean;
+    email?: string;
+    banned: boolean;
     /** @isInt @format int8 */
-    selected_badge?: number,
-    token: string,
+    selected_badge?: number;
+    token: string;
 }
 
 interface UserCredentials {
-    username: string,
-    password: string,
+    username: string;
+    password: string;
+    notARobot: number;
 }
 
 interface ResetRequestParams {
-    username: string,
-    email: string,
+    username: string;
+    email: string;
 }
 
 interface FinalizePassResetParams {
-    username: string,
-    token: string,
-    password: string,
+    username: string;
+    token: string;
+    password: string;
 }
 
 interface APIError {
-    error: string,
+    error: string;
 }
 
 @Route("auth")
@@ -74,31 +74,33 @@ export class AuthController extends Controller {
         const database = new Database();
         try {
             // TODO:stop grabbing phash2 or is_admin here
-            const users = await database.query('SELECT id,name,phash2,is_admin as isAdmin FROM User WHERE name = ?', [username]);
+            const users = await database.query("SELECT id,name,phash2,is_admin as isAdmin FROM User WHERE name = ?", [
+                username,
+            ]);
             if (users.length == 0) {
                 this.setStatus(401);
-                return { error: 'user does not exist' };
+                return { error: "user does not exist" };
             }
             const user = users[0];
-            const verified = await auth.verifyPassword(user.phash2, password)
+            const verified = await auth.verifyPassword(user.phash2, password);
 
             if (!verified) {
                 const u = await datastore.getUser(user.id);
                 datastore.updateUser({
                     id: user.id,
-                    unsuccessfulLogins: u.unsuccessfulLogins + 1
+                    unsuccessfulLogins: u.unsuccessfulLogins + 1,
                 });
                 this.setStatus(401);
-                return { error: 'bad credentials' };
+                return { error: "bad credentials" };
             } else {
                 datastore.updateUser({
                     id: user.id,
-                    dateLastLogin: moment().format('YYYY-MM-DD HH:mm:ss'),
-                    lastIp: this.getHeaders()['x-forwarded-for'],
-                    unsuccessfulLogins: 0
+                    dateLastLogin: moment().format("YYYY-MM-DD HH:mm:ss"),
+                    lastIp: this.getHeaders()["x-forwarded-for"],
+                    unsuccessfulLogins: 0,
                 });
                 user.token = auth.getToken(user.name, user.id, user.isAdmin);
-                this.setHeader('token', user.token);
+                this.setHeader("token", user.token);
                 return user;
             }
         } finally {
@@ -114,16 +116,18 @@ export class AuthController extends Controller {
     @Response<APIError>(400, "Invalid Username")
     @Post("reset-request")
     public async postResetRequest(@Body() requestBody: ResetRequestParams): Promise<void> {
-
-        const token = crypto.randomBytes(126).toString('hex');
+        const token = crypto.randomBytes(126).toString("hex");
 
         const database = new Database();
         try {
-            const results = await database.query('SELECT reset_token_set_time FROM User WHERE name = ? AND email = ?', [requestBody.username, requestBody.email])
+            const results = await database.query("SELECT reset_token_set_time FROM User WHERE name = ? AND email = ?", [
+                requestBody.username,
+                requestBody.email,
+            ]);
 
             // if no user by name, return OK anyway
             if (results.length === 0) return this.setStatus(204);
-            const result = results[0]
+            const result = results[0];
 
             // because we sent a email on each request,
             // this could be used to involentarily spam
@@ -131,15 +135,17 @@ export class AuthController extends Controller {
             // upon by email providers. so we only allow
             // resets every hour.
             const rsts = result.reset_token_set_time;
-            if (rsts && moment(rsts).isAfter(moment().subtract(1, 'hours'))) {
+            if (rsts && moment(rsts).isAfter(moment().subtract(1, "hours"))) {
                 console.log(`Attempt to reset password too quickly for ${requestBody.username}!`);
                 this.setStatus(425);
-                return { error: "too soon to reset, try later" }
+                return { error: "too soon to reset, try later" };
             }
 
             await database.execute(
                 `UPDATE User SET reset_token = ?, reset_token_set_time = CURRENT_TIMESTAMP
-      WHERE name = ?`, [token, requestBody.username]);
+      WHERE name = ?`,
+                [token, requestBody.username],
+            );
 
             let transporter = nodemailer.createTransport(config.smtp);
             // TODO: no html email!!! email was designed for plaintext only!
@@ -195,24 +201,26 @@ export class AuthController extends Controller {
             //sendmail is being called non-synchronously here (without await) because it can take a second
             //we're not telling the client anything different whether it succeeds or fails
             //so just send the 204 at this point
-            transporter.sendMail({
-                from: "webmaster@delicious-fruit.com",
-                to: requestBody.email,
-                subject: `Delicious-Fruit Password Reset`,
-                html,
-                text: `Greetings from Delicious Fruit!\n
+            transporter
+                .sendMail({
+                    from: "webmaster@delicious-fruit.com",
+                    to: requestBody.email,
+                    subject: `Delicious-Fruit Password Reset`,
+                    html,
+                    text: `Greetings from Delicious Fruit!\n
 A password reset request was made on your behalf. If this wasn't you, you can safely ignore this message.\n
 To reset your password, visit this link: http://delicious-fruit.com/password_reset.php?name=${requestBody.username}&token=${token} \n
 This link is valid for 2 hours since making the request.\n
--The staff at Delicious-Fruit ❤️`
-            }).then(mailResult => {
-                console.log(mailResult);
-            }).catch(err => {
-                console.log("Error sending mail!");
-                console.log(err);
-            });
+-The staff at Delicious-Fruit ❤️`,
+                })
+                .then((mailResult) => {
+                    console.log(mailResult);
+                })
+                .catch((err) => {
+                    console.log("Error sending mail!");
+                    console.log(err);
+                });
             return this.setStatus(204);
-
         } finally {
             database.close();
         }
@@ -220,8 +228,8 @@ This link is valid for 2 hours since making the request.\n
 
     /**
      * Should be called with the token the user received in their reset email. Generates a token after successful completion.
- *     @summary Finalize Password Reset
-    * */
+     *     @summary Finalize Password Reset
+     * */
     @SuccessResponse(200, "Password Reset")
     @Response<APIError>(401, "Bad Username/Token Pair")
     @Response<APIError>(422, "Unusable Token")
@@ -229,24 +237,31 @@ This link is valid for 2 hours since making the request.\n
     public async postFinalizePassReset(@Body() requestBody?: FinalizePassResetParams): Promise<AuthResponse> {
         const database = new Database();
         try {
-            const results = await database.query(`
+            const results = await database.query(
+                `
     SELECT id,reset_token_set_time,is_admin,reset_token_set_time FROM User 
-    WHERE name = ? AND reset_token = ?`, [requestBody.username, requestBody.token])
+    WHERE name = ? AND reset_token = ?`,
+                [requestBody.username, requestBody.token],
+            );
             if (results.length == 0) {
                 this.setStatus(401);
-                return { error: "no matching reset request with provided name and token" }
+                return { error: "no matching reset request with provided name and token" };
             }
             if (results.length > 1) {
-                console.log(`retrieved multiple users! username:[${requestBody.username}] token:[${requestBody.token}]`);
+                console.log(
+                    `retrieved multiple users! username:[${requestBody.username}] token:[${requestBody.token}]`,
+                );
                 this.setStatus(422);
-                return { error: "token is unusable, generate a new one" }
+                return { error: "token is unusable, generate a new one" };
             }
 
-            if (results[0].reset_token_set_time
-                && moment(results[0].reset_token_set_time).isBefore(moment().subtract(2, 'hours'))) {
+            if (
+                results[0].reset_token_set_time &&
+                moment(results[0].reset_token_set_time).isBefore(moment().subtract(2, "hours"))
+            ) {
                 console.log(`Attempted to use an old reset token for ${requestBody.username}`);
                 this.setStatus(422);
-                return { error: "token was already used" }
+                return { error: "token was already used" };
             }
 
             const phash = await auth.hashPassword(requestBody.password);
@@ -259,26 +274,33 @@ This link is valid for 2 hours since making the request.\n
       reset_token_set_time = null,
       ali_token = null,
       ali_date_set = null
-    WHERE id = ? `, [phash, results[0].id]);
+    WHERE id = ? `,
+                [phash, results[0].id],
+            );
 
             //get user for login
             // TODO: don't grab phash2 or isAdmin
-            const users = await database.query('SELECT id,name,phash2,is_admin as isAdmin FROM User WHERE name = ?', [requestBody.username]);
+            const users = await database.query("SELECT id,name,phash2,is_admin as isAdmin FROM User WHERE name = ?", [
+                requestBody.username,
+            ]);
             // QUEST: i think the predicate below has a 0% chance of ever happening, right?
             if (users.length == 0) {
-                this.setStatus(401)
-                return { error: 'invalid credentials' };
+                this.setStatus(401);
+                return { error: "invalid credentials" };
             }
             const user = users[0];
             user.token = auth.getToken(user.name, user.id, user.isAdmin);
 
-            datastore.addReport({
-                type: "user_password_change",
-                targetId: "" + user.id,
-                report: "User password changed"
-            }, user.id);
+            datastore.addReport(
+                {
+                    type: "user_password_change",
+                    targetId: "" + user.id,
+                    report: "User password changed",
+                },
+                user.id,
+            );
 
-            this.setHeader('token', user.token);
+            this.setHeader("token", user.token);
             return user;
         } finally {
             database.close();
@@ -298,36 +320,31 @@ This link is valid for 2 hours since making the request.\n
             token = extractBearerJWT(authorization);
         } catch (e) {
             this.setStatus(401);
-            return { error: "expired or invalid token" }
+            return { error: "expired or invalid token" };
         }
         const user = await datastore.getUser(token.sub);
         if (!user) {
             this.setStatus(401);
-            return { error: "specified user with this token does not exist" }
+            return { error: "specified user with this token does not exist" };
         }
         user.token = auth.getToken(user.name, user.id, user.isAdmin);
-        this.setHeader('token', user.token);
+        this.setHeader("token", user.token);
         return user;
     }
 }
 
-
-
-
-
 function extractBearerJWT(header_token: string): string | object {
     if (!header_token.includes("Bearer ")) {
-        throw new Error("missing prefix")
+        throw new Error("missing prefix");
     }
     const unverified_token = header_token.split(" ")[1];
 
     try {
         return jwt.verify(unverified_token, config.app_jwt_secret);
     } catch (e) {
-        throw new Error(`invalid token: ${e}`)
+        throw new Error(`invalid token: ${e}`);
     }
 }
-
 
 export async function recaptchaVerify(action: string, token: string, remoteIp?: string): Promise<boolean> {
     if (!config.recaptcha_secret) {
@@ -335,30 +352,30 @@ export async function recaptchaVerify(action: string, token: string, remoteIp?: 
         return true;
     }
     const request: any = {
-        'secret': config.recaptcha_secret,
-        'response': token,
+        secret: config.recaptcha_secret,
+        response: token,
     };
     if (remoteIp) request.remoteip = remoteIp;
     try {
         const rsp = await axios.post("https://www.google.com/recaptcha/api/siteverify", request);
         if (!rsp.data.success) {
-            console.log("reCaptcha: Invalid token!")
-            console.log(rsp.data)
+            console.log("reCaptcha: Invalid token!");
+            console.log(rsp.data);
             return false;
         }
         if (rsp.data.action != action) {
-            console.log("reCaptcha: Action doesn't match expected action! expected: " + action)
-            console.log(rsp.data)
+            console.log("reCaptcha: Action doesn't match expected action! expected: " + action);
+            console.log(rsp.data);
             return false;
         }
         if (rsp.data.score < config.recaptcha_threshold) {
-            console.log("reCaptcha: score under threshold!")
-            console.log(rsp.data)
+            console.log("reCaptcha: score under threshold!");
+            console.log(rsp.data);
             return false;
         }
         return true;
     } catch (err) {
-        console.log('recaptcha verify error! allowing request')
+        console.log("recaptcha verify error! allowing request");
         console.log(err);
         return true;
     }
