892657325019b72fa8d71b57196c7ec92fb1c9d0
Starz0r <starz0r@starz0r.com>
2025-09-12 13:53:29 -0400

    message-router: when sending a message, select all related threads

--- src/message-router.ts
+++ src/message-router.ts
@@ -1,29 +1,29 @@
-import express from 'express';
-import { Database } from './database';
-import InsertList from './lib/insert-list';
-import WhereList from './lib/where-list';
-import { Message } from './model/Message';
-import { MessageQueryParams } from './model/MessageQueryParams';
-import handle from './lib/express-async-catch';
-import { userCheck } from './lib/auth-check';
-import { Body, Controller, Get, Header, Path, Post, Route, Security, SuccessResponse, Tags } from 'tsoa';
+import express from "express";
+import { Database } from "./database";
+import InsertList from "./lib/insert-list";
+import WhereList from "./lib/where-list";
+import { Message } from "./model/Message";
+import { MessageQueryParams } from "./model/MessageQueryParams";
+import handle from "./lib/express-async-catch";
+import { userCheck } from "./lib/auth-check";
+import { Body, Controller, Get, Header, Path, Post, Route, Security, SuccessResponse, Tags } from "tsoa";
 
 const app = express.Router();
 export default app;
 
 import * as jwt from "jsonwebtoken";
-import Config from './model/config';
-let config: Config = require('./config/config.json');
+import Config from "./model/config";
+let config: Config = require("./config/config.json");
 function extractBearerJWT(header_token: string): string | object {
     if (!header_token.includes("Bearer ")) {
-        throw new Error("missing prefix")
+        throw new Error("missing prefix");
     }
     const unverified_token = header_token.split(" ")[1];
 
     try {
         return jwt.verify(unverified_token, config.app_jwt_secret);
     } catch (e) {
-        throw new Error(`invalid token: ${e}`)
+        throw new Error(`invalid token: ${e}`);
     }
 }
 
@@ -45,10 +45,13 @@ export class MessageController extends Controller {
 
         const database = new Database();
         try {
-            const messages = await database.query(`
+            const messages = await database.query(
+                `
       SELECT *, thread_id as threadId
       FROM Message 
-      ${whereList.getClause()}`, whereList.getParams());
+      ${whereList.getClause()}`,
+                whereList.getParams(),
+            );
             return messages;
         } finally {
             database.close();
@@ -58,7 +61,10 @@ export class MessageController extends Controller {
     @Security("bearerAuth", ["user"])
     @SuccessResponse(200, "List of Messages from thread")
     @Get("thread/{id}")
-    public async getMessagesFromThread(@Header("Authorization") authorization: string, @Path() id: number): Promise<Message[]> {
+    public async getMessagesFromThread(
+        @Header("Authorization") authorization: string,
+        @Path() id: number,
+    ): Promise<Message[]> {
         // NOTE: auth guard should make the error condition unreachable
         const user = extractBearerJWT(authorization);
 
@@ -72,21 +78,26 @@ export class MessageController extends Controller {
 
         const database = new Database();
         try {
-            const messages = await database.query(`
+            const messages = await database.query(
+                `
       SELECT *
       FROM Message 
-      ${whereList.getClause()}`, whereList.getParams());
+      ${whereList.getClause()}`,
+                whereList.getParams(),
+            );
             return messages;
         } finally {
             database.close();
         }
-
     }
 
     @Security("bearerAuth", ["user"])
     @SuccessResponse(201, "Sent Message")
     @Post()
-    public async postMessage(@Header("Authorization") authorization: string, @Body() requestBody: Message): Promise<void> {
+    public async postMessage(
+        @Header("Authorization") authorization: string,
+        @Body() requestBody: Message,
+    ): Promise<void> {
         // NOTE: auth guard should make the error condition unreachable
         const user = extractBearerJWT(authorization);
 
@@ -99,21 +110,33 @@ export class MessageController extends Controller {
 
         const database = new Database();
         try {
-            let result = await database.execute(`
-      INSERT INTO Message ${insertList.getClause()}`, insertList.getParams());
-            if (!requestBody.replyToId) { //new thread
-                result = await database.execute(`
-        UPDATE Message SET thread_id = id WHERE id = ?`, result.insertId);
-                if (result.affectedRows != 1) throw 'Wonky database error!';
+            let result = await database.execute(
+                `
+      INSERT INTO Message ${insertList.getClause()}`,
+                insertList.getParams(),
+            );
+            if (!requestBody.replyToId) {
+                //new thread
+                result = await database.execute(
+                    `
+        UPDATE Message SET thread_id = id WHERE id = ?`,
+                    result.insertId,
+                );
+                if (result.affectedRows != 1) throw "Wonky database error!";
             } else {
-                let replyTo = await database.query(`
-        SELECT thread_id FROM Message WHERE id = ?`, result.replyToId);
+                let replyTo = await database.query(
+                    `
+ SELECT thread_id FROM Message WHERE id = ?`,
+                    [requestBody.replyToId],
+                );
                 if (replyTo.length != 1) {
-
                 }
-                result = await database.execute(`
-        UPDATE Message SET thread_id = ? WHERE id = ?`, [replyTo[0].thread_id, result.insertId]);
-                if (result.affectedRows != 1) throw 'Wonky database error!';
+                result = await database.execute(
+                    `
+        UPDATE Message SET thread_id = ? WHERE id = ?`,
+                    [replyTo[0].thread_id, result.insertId],
+                );
+                if (result.affectedRows != 1) throw "Wonky database error!";
             }
 
             this.setStatus(201);
@@ -121,6 +144,5 @@ export class MessageController extends Controller {
         } finally {
             database.close();
         }
-
     }
 }
