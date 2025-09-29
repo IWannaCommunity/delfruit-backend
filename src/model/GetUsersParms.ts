44469035bc41d78de81eae5666b71fb4208603e6
Starz0r <starz0r@starz0r.com>
2025-09-29 03:33:18 -0400

    model//GetUsersParms: mark as deprecated
    Some of these types are misspelled too, LOL

--- src/model/GetUsersParms.ts
+++ src/model/GetUsersParms.ts
@@ -1,11 +1,15 @@
+/**
+ * @deprecated Use /model/user/GetUsersParams instead
+ */
 export interface GetUsersParms {
-  page: number;
-  limit: number;
-  orderCol?: string;
-  orderDir?: "ASC" | "DESC";
+	page: number;
+	limit: number;
+	orderCol?: string;
+	orderDir?: "ASC" | "DESC";
 
-  id?: number;
-  followerUserId?: number;
-  banned?: boolean;
-  name?: string;
+	id?: number;
+	followerUserId?: number;
+	banned?: boolean;
+	name?: string;
 }
+
