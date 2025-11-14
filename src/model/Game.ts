4e77bf3902d87da43cc5b53a08a3016521f2cafb
Starz0r <starz0r@starz0r.com>
2025-11-14 16:00:12 -0500

    model/Game: deprecation notice

--- src/model/Game.ts
+++ src/model/Game.ts
@@ -1,15 +1,18 @@
+/**
+ * @deprecated Use \model\response\game instead.
+ */
 export interface Game {
-  id?: number;
-  name?: string;
-  sortname?: string;
-  url?: string;
-  urlSpdrn?: string;
-  author?: string[];
-  author_raw?: string;
-  collab?: boolean;
-  dateCreated?: string;
-  adderId?: string;
-  removed?: boolean;
-  ownerId?: string;
-  ownerBio?: string;
+	id?: number;
+	name?: string;
+	sortname?: string;
+	url?: string;
+	urlSpdrn?: string;
+	author?: string[];
+	author_raw?: string;
+	collab?: boolean;
+	dateCreated?: string;
+	adderId?: string;
+	removed?: boolean;
+	ownerId?: string;
+	ownerBio?: string;
 }
