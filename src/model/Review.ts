4f450bf160c9064ad2e929e6bd1e96710f522a7e
Starz0r <starz0r@starz0r.com>
2026-01-13 02:46:44 -0500

    model/Review: expand to include game tags

--- src/model/Review.ts
+++ src/model/Review.ts
@@ -1,10 +1,11 @@
 export interface Review {
-  id?: number;
-  user_id?: number;
-  game_dd?: number;
-  rating?: number;
-  difficulty?: number;
-  comment?: string;
-  date_created?: string;
-  removed?: boolean;
+	id?: number;
+	user_id?: number;
+	game_dd?: number;
+	rating?: number;
+	difficulty?: number;
+	comment?: string;
+	date_created?: string;
+	removed?: boolean;
+	tags?: Array<number>;
 }
