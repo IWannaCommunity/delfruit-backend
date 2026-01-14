a7a145049d826889ca208c64e33e3469ac38fbf1
Starz0r <starz0r@starz0r.com>
2026-01-14 14:36:05 -0500

    lib/security: inject decoded auth from the header into the request

--- src/lib/security.ts
+++ src/lib/security.ts
@@ -1,63 +1,100 @@
-import { userCheck, adminCheck, AuthnCheck } from "./auth-check";
+import { Request, type Response } from "express";
 import * as jwt from "jsonwebtoken";
-import { Request, Response } from "express";
+import type { RequestExt } from "../model/app/request";
+import { AuthnCheck, adminCheck, userCheck } from "./auth-check";
 
-let config: Config = require('../config/config.json');
+const config: Config = require("../config/config.json");
 
-export function expressAuthentication(req: Request, securityName: string, scopes?: string[]): Promise<object | Response> {
-    // only supports bearerAuth security scheme
-    if ((securityName !== "bearerAuth") || (!scopes)) {
-        return Promise.reject();
-    }
+function extractBearerJWT(header_token: string): string | object {
+	try {
+		return jwt.verify(header_token, config.app_jwt_secret);
+	} catch (e) {
+		throw new Error(`invalid token: ${e}`);
+	}
+}
 
-    // we also need the response object to properly return errors
-    if (!req.res) {
-        return Promise.reject({});
-    }
-    const resp = req.res;
+export function expressAuthentication(
+	req: RequestExt,
+	securityName: string,
+	scopes?: string[],
+): Promise<object | Response> {
+	// only supports bearerAuth security scheme
+	if (securityName !== "bearerAuth" || !scopes) {
+		return Promise.reject();
+	}
 
-    // HACK: unfortunately, we need to manually split the header in two to get the token
-    if (!req.headers["authorization"] || !req.headers["authorization"].includes("Bearer ")) {
-        return Promise.reject(
-            resp.status(401).contentType("application/json").send({ error: "bearer auth malformed or missing in header" })
-        );
-    }
-    const unverified_token = req.headers["authorization"].split(" ")[1];
+	// we also need the response object to properly return errors
+	if (!req.res) {
+		return Promise.reject({});
+	}
+	const resp = req.res;
 
-    let token = null;
-    try {
-        token = jwt.verify(unverified_token, config.app_jwt_secret);
-    } catch (e) {
-        // TODO: log e, do NOT return it to the user
-        return Promise.reject(
-            resp.status(401).contentType("application/json").send({ error: "invalid token" })
-        );
-    }
+	if (!req.headers.authorization.includes("Bearer ")) {
+		return Promise.reject(
+			resp
+				.status(401)
+				.contentType("application/json")
+				.send({ error: "missing bearer prefix" }),
+		);
+	}
+
+	// HACK: unfortunately, we need to manually split the header in two to get the token
+	if (!req.headers["authorization"]) {
+		return Promise.reject(
+			resp
+				.status(401)
+				.contentType("application/json")
+				.send({ error: "bearer auth malformed or missing in header" }),
+		);
+	}
+	const unverified_token = req.headers["authorization"].split(" ")[1];
+
+	let token = null;
+	try {
+		token = extractBearerJWT(unverified_token);
+	} catch (e) {
+		// TODO: log e, do NOT return it to the user
+		return Promise.reject(
+			resp
+				.status(401)
+				.contentType("application/json")
+				.send({ error: "invalid token" }),
+		);
+	}
 
 	// HACK: actually check scopes instead of whatever the fuck is happen below here
-	if ((scopes) && (scopes[0] === "user")) {
+	if (scopes && scopes[0] === "user") {
 		if (userCheck(token) === AuthnCheck.Authorized) {
+			req.app_user = token;
 			return Promise.resolve({});
 		}
 	}
-	
-    if (token.isAdmin === false) {
-        if (userCheck(token) === AuthnCheck.Authorized) {
-            return Promise.resolve({});
-        } else {
-            return Promise.reject(
-                resp.status(403).contentType("application/json").send({ error: "not signed in" })
-            );
-        }
-    } else {
-        if (adminCheck(token) === AuthnCheck.Authorized) {
-            return Promise.resolve({});
-        } else {
-            return Promise.reject(
-                resp.status(403).contentType("application/json").send({ error: "endpoint requires admin permission" })
-            );
-        }
-    }
 
-    return Promise.reject({ error: "" });
+	if (token.isAdmin === false) {
+		if (userCheck(token) === AuthnCheck.Authorized) {
+			req.app_user = token;
+			return Promise.resolve({});
+		} else {
+			return Promise.reject(
+				resp
+					.status(403)
+					.contentType("application/json")
+					.send({ error: "not signed in" }),
+			);
+		}
+	} else {
+		if (adminCheck(token) === AuthnCheck.Authorized) {
+			req.app_user = token;
+			return Promise.resolve({});
+		} else {
+			return Promise.reject(
+				resp
+					.status(403)
+					.contentType("application/json")
+					.send({ error: "endpoint requires admin permission" }),
+			);
+		}
+	}
+
+	return Promise.reject({ error: "" });
 }
