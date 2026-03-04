e0d391d8361634b5da65945cbb9b957aecb9cd2d
Starz0r <starz0r@starz0r.com>
2026-03-03 19:07:01 -0500

    config: expand configuration options

--- src/model/config.ts
+++ src/model/config.ts
@@ -1,35 +1,50 @@
-import mysql from 'mysql2';
-import * as Minio from 'minio';
-
+import type * as Minio from "minio";
+import type mysql from "mysql2";
 
 export default interface Config {
-    db: mysql.ConnectionOptions;
+	db: mysql.ConnectionOptions;
 
-    app_port: number;
-    app_jwt_secret: string;
-    bcrypt_rounds: number;
+	app_port: number;
+	app_jwt_secret: string;
+	bcrypt_rounds: number;
 
-    s3: Minio.ClientOptions;
-    s3_bucket: string;
-    s3_region: string;
+	s3: Minio.ClientOptions;
+	s3_bucket: string;
+	s3_region: string;
 
-    smtp: {
-        host: string;
-        port: number;
-        secure: boolean;
-        auth: {
-            user: string;
-            pass: string;
-        }
-    },
-    recaptcha_threshold: number;
-    recaptcha_secret: string;
+	smtp: {
+		host: string;
+		port: number;
+		secure: boolean;
+		auth: {
+			user: string;
+			pass: string;
+		};
+	};
+	recaptcha_threshold: number;
+	recaptcha_secret: string;
 
-    memcache: {
-        hosts: string[];
-        /** options from https://www.npmjs.com/package/memcached */
-        options: {
-            maxExpiration: number
-        }
-    }
+	memcache: {
+		hosts: string[];
+		/** options from https://www.npmjs.com/package/memcached */
+		options: {
+			maxExpiration: number;
+		};
+	};
+
+	logLevel:
+		| "trace"
+		| "debug"
+		| "warn"
+		| "info"
+		| "error"
+		| "err"
+		| "fatal"
+		| "wtf";
+	initDatabase: boolean;
+	captcha: {
+		cfTurnstileSecret: string;
+	};
+	registrationsEnabled: boolean;
+	loginsEnabled: boolean;
 }
