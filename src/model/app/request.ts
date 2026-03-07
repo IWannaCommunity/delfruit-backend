2b2c367adc0d0b37224516666e04a94030df61fd
Starz0r <starz0r@starz0r.com>
2026-03-06 20:26:09 -0500

    model/app/request: field for application configuration

--- src/model/app/request.ts
+++ src/model/app/request.ts
@@ -1,8 +1,10 @@
 import { type Request, Response } from "express";
 import type { StdLogger } from "../../logger";
+import type Config from "../../repository/config";
 import type { UserJWT } from "./userjwt";
 
 export interface RequestExt extends Request {
 	logger: StdLogger;
 	app_user?: UserJWT;
+	appConfig: Config;
 }
