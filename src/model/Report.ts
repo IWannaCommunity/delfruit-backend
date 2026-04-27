6e742f713d949d7fa20445db3d4868873c5175dd
Starz0r <starz0r@starz0r.com>
2026-04-27 19:09:11 -0400

    model/Report: restore missing report types

--- src/model/Report.ts
+++ src/model/Report.ts
@@ -1,28 +1,33 @@
 export interface Report {
-  id?: number;
-  type?: "game"
-        |"game_add"
-        |"game_remove"
-        |"game_update_url"
-        |"game_update_owner"
-        |"game_update_creator"
-        |"game_update"
-        
-        |"user"
-        |"user_register"
-        |"user_password_change"
-        
-        |"review"
-        |"review_restore"
-        
-        |"screenshot"
-        |"screenshot_remove";
-  targetId?: number;
-  report?: string;
-  reporterId?: number;
-  reporterName?: string;
-  answeredById?: number;
-  answeredByName?: string;
-  dateCreated?: string;
-  dateAnswered?: string;
+	id?: number;
+	type?:
+		| "game"
+		| "game_add"
+		| "game_remove"
+		| "game_update_url"
+		| "game_update_owner"
+		| "game_update_creator"
+		| "game_update"
+		| "game_restored"
+		| "user"
+		| "user_register"
+		| "user_password_change"
+		| "review"
+		| "review_restore"
+		| "review_reported"
+		| "review_removed"
+		| "screenshot"
+		| "screenshot_remove"
+		| "screenshot_added"
+		| "screenshot_reported"
+		| "screenshot_approved"
+		| "screenshot_denied";
+	targetId?: number;
+	report?: string;
+	reporterId?: number;
+	reporterName?: string;
+	answeredById?: number;
+	answeredByName?: string;
+	dateCreated?: string;
+	dateAnswered?: string;
 }
