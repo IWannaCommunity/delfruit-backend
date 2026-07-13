494793ceff7043ee446f6f468fc3da9ebc6cc0ce
Starz0r <starz0r@starz0r.com>
2026-07-12 20:17:23 -0400

    controllers/list: allow legacy lists to be interacted with

--- src/controllers/list.controller.ts
+++ src/controllers/list.controller.ts
@@ -1,19 +1,20 @@
 import express from "express";
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Header,
-    Patch,
-    Path,
-    Post,
-    Put,
-    Response,
-    Route,
-    Security,
-    SuccessResponse,
-    Tags,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Header,
+	Patch,
+	Path,
+	Post,
+	Put,
+	Request,
+	Response,
+	Route,
+	Security,
+	SuccessResponse,
+	Tags,
 } from "tsoa";
 import datastore from "../datastore";
 import { userCheck } from "../lib/auth-check";
@@ -28,130 +29,131 @@ import type Config from "../model/config";
 const config: Config = require("../config/config.json");
 
 import * as jwt from "jsonwebtoken";
-import { Game } from "../model/Game";
-import { APIError } from "../model/response/error";
+import type { RequestExt } from "../model/app/request";
+import type { Game } from "../model/Game";
+import type { APIError } from "../model/response/error";
 
 function extractBearerJWT(header_token: string): string | object {
-    if (!header_token.includes("Bearer ")) {
-        throw new Error("missing prefix");
-    }
-    const unverified_token = header_token.split(" ")[1];
+	if (!header_token.includes("Bearer ")) {
+		throw new Error("missing prefix");
+	}
+	const unverified_token = header_token.split(" ")[1];
 
-    try {
-        return jwt.verify(unverified_token, config.app_jwt_secret);
-    } catch (e) {
-        throw new Error(`invalid token: ${e}`);
-    }
+	try {
+		return jwt.verify(unverified_token, config.app_jwt_secret);
+	} catch (e) {
+		throw new Error(`invalid token: ${e}`);
+	}
 }
 
 @Tags("Lists")
 @Route("lists")
 export class ListController extends Controller {
-    @Security("bearerAuth", ["user"])
-    @SuccessResponse(201, "Created")
-    @Post()
-    public async postList(
-        @Header("Authorization") authorization: string,
-        @Body() requestBody: List,
-    ): Promise<List> {
-        // NOTE: auth guard should make the error condition unreachable
-        const user = extractBearerJWT(authorization);
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(201, "Created")
+	@Post()
+	public async postList(
+		@Header("Authorization") authorization: string,
+		@Body() requestBody: List,
+	): Promise<List> {
+		// NOTE: auth guard should make the error condition unreachable
+		const user = extractBearerJWT(authorization);
 
-        const list = await datastore.addList(requestBody, user.sub);
-        return list;
-    }
+		const list = await datastore.addList(requestBody, user.sub);
+		return list;
+	}
 
-    /**
-     * Update List (User/Admin Only)
-     * @summary Update List (User/Admin Only)
-     */
-    @Security("bearerAuth", ["user"])
-    @SuccessResponse(200, "Updated Successfully")
-    @Patch("{id}")
-    public async patchList(
-        @Header("Authorization") authorization: string,
-        @Path() id: number,
-    ): Promise<void> {
-        // TODO: currently unimplemented
-    }
+	/**
+	 * Update List (User/Admin Only)
+	 * @summary Update List (User/Admin Only)
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(200, "Updated Successfully")
+	@Patch("{id}")
+	public async patchList(
+		@Header("Authorization") authorization: string,
+		@Path() id: number,
+	): Promise<void> {
+		// TODO: currently unimplemented
+	}
 
-    /**
-     * Get List
-     * @summary Get List
-     */
-    @SuccessResponse(200, "Found")
-    @Get("{id}")
-    public async getList(
-        @Header("Authorization") authorization: string,
-        @Path() id: number,
-    ): Promise<void> {
-        // TODO: currently unimplemented
-    }
+	/**
+	 * Get List
+	 * @summary Get List
+	 */
+	@SuccessResponse(200, "Found")
+	@Get("{id}")
+	public async getList(
+		@Header("Authorization") authorization: string,
+		@Path() id: number,
+	): Promise<void> {
+		// TODO: currently unimplemented
+	}
 
-    /**
-     * Get List Contents
-     * @summary Get List Contents
-     */
-    @SuccessResponse(200, "Found")
-    @Get("{id}/games")
-    public async getListGames(
-        @Header("Authorization") authorization: string,
-        @Path() id: number,
-    ): Promise<void> {
-        // TODO: currently unimplemented
-    }
+	/**
+	 * Get List Contents
+	 * @summary Get List Contents
+	 */
+	@SuccessResponse(200, "Found")
+	@Get("{id}/games")
+	public async getListGames(
+		@Header("Authorization") authorization: string,
+		@Path() id: number,
+	): Promise<void> {
+		// TODO: currently unimplemented
+	}
 
-    /**
-     * Replace Games in List (User/Admin Only)
-     * @summary Replace Games in List (User/Admin Only)
-     */
-    @Security("bearerAuth", ["user"])
-    @SuccessResponse(204, "Updated Successfully")
-    @Response<void>(403, "Bad Ownership")
-    @Response<void>(404, "Not Found")
-    @Put("{id}/games")
-    public async putList(
-        @Header("Authorization") authorization: string,
-        @Path() id: number,
-        @Body() requestBody: any,
-    ): Promise<void> {
-        // NOTE: auth guard should make the error condition unreachable
-        const user = extractBearerJWT(authorization);
+	/**
+	 * Replace Games in List (User/Admin Only)
+	 * @summary Replace Games in List (User/Admin Only)
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Updated Successfully")
+	@Response<void>(403, "Bad Ownership")
+	@Response<void>(404, "Not Found")
+	@Put("{id}/games")
+	public async putList(
+		@Header("Authorization") authorization: string,
+		@Path() id: number,
+		@Body() requestBody: any,
+	): Promise<void> {
+		// NOTE: auth guard should make the error condition unreachable
+		const user = extractBearerJWT(authorization);
 
-        const list = await datastore.getList(id);
-        if (!list) return this.setStatus(404);
+		const list = await datastore.getList(id);
+		if (!list) return this.setStatus(404);
 
-        if (list.userId !== user.sub) return this.setStatus(403);
+		if (list.userId !== user.sub) return this.setStatus(403);
 
-        const games = await datastore.getListGames(id);
-        if (games.includes(requestBody.gameId)) return this.setStatus(204);
+		const games = await datastore.getListGames(id);
+		if (games.includes(requestBody.gameId)) return this.setStatus(204);
 
-        await datastore.addGameToList(id, requestBody.gameId);
-        return this.setStatus(204);
-    }
+		await datastore.addGameToList(id, requestBody.gameId);
+		return this.setStatus(204);
+	}
 
-    /**
-     * Delete List (User/Admin Only)
-     * @summary Delete List (User/Admin Only)
-     */
-    @Security("bearerAuth", ["user"])
-    @SuccessResponse(200, "Deleted")
-    @Response<void>(403, "Bad Ownership")
-    @Response<void>(404, "Not Found")
-    @Delete("{id}")
-    public async deleteList(
-        @Header("Authorization") authorization: string,
-        @Path() id: number,
-    ): Promise<void> {
-        // don't allow deleting lists 1,2 - favorites and clears
-        // TODO: unimplemented
-    }
+	/**
+	 * Delete List (User/Admin Only)
+	 * @summary Delete List (User/Admin Only)
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(200, "Deleted")
+	@Response<void>(403, "Bad Ownership")
+	@Response<void>(404, "Not Found")
+	@Delete("{id}")
+	public async deleteList(
+		@Header("Authorization") authorization: string,
+		@Path() id: number,
+	): Promise<void> {
+		// don't allow deleting lists 1,2 - favorites and clears
+		// TODO: unimplemented
+	}
 
-    /**
-     * Get Games from Owner
-     * @summary Get Games from Owner
-     */
-    @SuccessResponse(200, "Found Game Details")
+	/**
+	 * Get Games from Owner
+	 * @summary Get Games from Owner
+	 */
+	@SuccessResponse(200, "Found Game Details")
     @Response<APIError>(400, "Unparsable ID")
     @Get("/owned/{ownerId}")
     public async getGamesByOwner(@Path() ownerId: number): Promise<Array<Game & { rating: number, difficulty: number, rating_count: number }>> {
@@ -159,11 +161,11 @@ export class ListController extends Controller {
         return await datastore.getGamesByOwner(ownerId);
     }
 
-    /**
-     * Get Favorites List Contents
-     * @summary Get Favorites List Contents
-     */
-    @SuccessResponse(200, "Found")
+	/**
+	 * Get Favorites List Contents
+	 * @summary Get Favorites List Contents
+	 */
+	@SuccessResponse(200, "Found")
     @Get("favorites/{uid}")
     public async getFavoritesListGames(
         @Path() uid: number,
@@ -179,11 +181,11 @@ export class ListController extends Controller {
         return await datastore.getUserFavorites(uid);
     }
 
-    /**
-     * Get Clear List Contents
-     * @summary Get Clear List Contents
-     */
-    @SuccessResponse(200, "Found")
+	/**
+	 * Get Clear List Contents
+	 * @summary Get Clear List Contents
+	 */
+	@SuccessResponse(200, "Found")
     @Get("clears/{uid}")
     public async getClearListGames(
         @Path() uid: number,
@@ -198,4 +200,154 @@ export class ListController extends Controller {
         this.setStatus(200);
         return await datastore.getUserClears(uid);
     }
+
+	/**
+	 * Add Game to Bookmarks
+	 * @summary Add Game to Bookmarks
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Bookmarked")
+	@Put("bookmark/{gid}")
+	public async putBookmarkGame(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<void> {
+		await datastore.bookmarkGame(Number(req.app_user.sub), gid);
+		this.setStatus(204);
+		return;
+	}
+
+	/**
+	 * Check if User has Game bookmarked
+	 * @summary Check if User has a Game bookmarked
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(200, "Bookmarked")
+	@Get("bookmark/{gid}")
+	public async getIsGameBookmarked(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<boolean> {
+		this.setStatus(200);
+		return datastore.isUserBookmarking(Number(req.app_user.sub), gid);
+	}
+
+	/**
+	 * Remove a Game from being bookmarked
+	 * @summary Remove a Game from being bookmarked
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Bookmark Removed")
+	@Delete("bookmark/{gid}")
+	public async deleteBookmarkedGame(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<void> {
+		this.setStatus(204);
+		await datastore.unbookmarkGame(Number(req.app_user.sub), gid);
+		return;
+	}
+
+	/**
+	 * Add Game to Favorites
+	 * @summary Add Game to Favorites
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Favorited")
+	@Put("favorite/{gid}")
+	public async putFavoriteGame(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<void> {
+		await datastore.favoriteGame(Number(req.app_user.sub), gid);
+		this.setStatus(204);
+		return;
+	}
+
+	/**
+	 * Check if User has Game favorited
+	 * @summary Check if User has a Game favorited
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(200, "Favorited")
+	@Get("favorite/{gid}")
+	public async getIsGameFavorited(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<boolean> {
+		this.setStatus(200);
+		return datastore.isUserFavoriting(Number(req.app_user.sub), gid);
+	}
+
+	/**
+	 * Remove a Game from being favorited
+	 * @summary Remove a Game from being favorited
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Favorite Removed")
+	@Delete("favorite/{gid}")
+	public async deleteFavoritedGame(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<void> {
+		this.setStatus(204);
+		datastore.unfavoriteGame(Number(req.app_user.sub), gid);
+		return;
+	}
+
+	/**
+	 * Add Game to Clear
+	 * @summary Add Game to Clear
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Cleared")
+	@Put("clear/{gid}")
+	public async putClearGame(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<void> {
+		await datastore.clearGame(Number(req.app_user.sub), gid);
+		this.setStatus(204);
+		return;
+	}
+
+	/**
+	 * Check if User has Game cleared
+	 * @summary Check if User has a Game cleared
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(200, "Cleared")
+	@Get("clear/{gid}")
+	public async getIsGameCleared(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<boolean> {
+		this.setStatus(200);
+		return datastore.isUserCleared(Number(req.app_user.sub), gid);
+	}
+
+	/**
+	 * Remove a Game from being cleared
+	 * @summary Remove a Game from being cleared
+	 */
+	@Security("bearerAuth", ["user"])
+	@SuccessResponse(204, "Clear Removed")
+	@Delete("clear/{gid}")
+	public async deleteClearedGame(
+		@Request() req: RequestExt,
+		@Header("Authorization") authorization: string,
+		@Path() gid: number,
+	): Promise<void> {
+		this.setStatus(204);
+		datastore.unclearGame(Number(req.app_user.sub), gid);
+		return;
+	}
 }
